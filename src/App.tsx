b3232a02f8a9fd943fb3be41066acf0d71594811
joaoiaronka622@gmail.com <joaoiaronka622@gmail.com>
2026-01-14 21:05:21 -0300

    Utilizando Hide boolean

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
 import { MyButton } from "./shared/Components/MyButton";
 import {  SafeAreaView } from 'react-native-safe-area-context'
-import { Text  } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 import { useState } from "react";
 
 export default function App() {
 
   // aprendendo use state
-  const [state, setState] = useState ('Teste');
-  
-
-  console.log("Teste")
+  const [hide, setHide] = useState (false);
 
 
   return(
   <SafeAreaView>
+
+    {hide ? <Text>Apareceu</Text> : <Text>Sumiu</Text> }
   
-   <MyButton order={1} onPress={() => setState('Teste2')}>
-      <Text>MyButton {state}</Text>
-   </MyButton>
-
-   <MyButton order={2} onPress={() => console.log('teste')}>
-      <Text>MyButton {2}</Text>
-    </MyButton>
-
-   <MyButton order={3} onPress={() => console.log('teste')}>
-      <Text>MyButton {3}</Text>
-    </MyButton>
-
-   <MyButton order={4} onPress={() => console.log('teste')}>
-      <Text>MyButton {4}</Text>
-    </MyButton>
+    <TouchableOpacity onPress = {() => setHide(!hide)} >
+      <Text>Toggle</Text>
+    </TouchableOpacity>
   
   </SafeAreaView>
 
