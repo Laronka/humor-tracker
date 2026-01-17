acd6a637a9ae04018bf505d7aa2c72cb0cfe34f4
joaoiaronka622@gmail.com <joaoiaronka622@gmail.com>
2026-01-16 21:02:55 -0300

    entendendo list map e sinxtas

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import { MyButton } from "./shared/Components/MyButton";
 import {  SafeAreaView } from 'react-native-safe-area-context'
 import { Text, TouchableOpacity } from 'react-native'
 import { useState } from "react";
+import { ScrollView } from 'react-native'
 
 export default function App() {
 
   // aprendendo use state
-  const [hide, setHide] = useState (false);
+  const [list, setList] = useState ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70,90,100,200,300,400,500,600,700
+    ,800,900,105,1000,1001,1002,1003,1004, 1007, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 100000
+  ]);
 
+  const [mostrar, setMostrar] = useState(false);
+
+  console.log(mostrar);
 
   return(
   <SafeAreaView>
 
-    {hide ? <Text>Apareceu</Text> : <Text>Sumiu</Text> }
-  
-    <TouchableOpacity onPress = {() => setHide(!hide)} >
-      <Text>Toggle</Text>
+    <TouchableOpacity onPress = {() => setMostrar(!mostrar)}>
+      <Text> Clique aqui </Text>
     </TouchableOpacity>
+    
+    {mostrar && (
+    <ScrollView>
+      {
+        list.map((item, index) => (<Text key={index}>valor: {item} posição: {index}</Text>))
+      }
+    </ScrollView>    
+    )}
   
   </SafeAreaView>
 
