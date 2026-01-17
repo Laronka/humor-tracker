c48d1c87c885da4337d024197c2c7d47eb0873ca
joaoiaronka622@gmail.com <joaoiaronka622@gmail.com>
2026-01-17 10:41:45 -0300

    trabalhando com primeiras estilizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {  SafeAreaView } from 'react-native-safe-area-context'
-import { FlatList, Text, TouchableOpacity } from 'react-native'
+import { View, FlatList, Pressable, StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { useState } from "react";
 import { ScrollView } from 'react-native'
 
@@ -7,9 +7,7 @@ import { ScrollView } from 'react-native'
 export default function App() {
 
   // aprendendo use state
-  const [list, setList] = useState ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70,90,100,200,300,400,500,600,700
-    ,800,900,105,1000,1001,1002,1003,1004, 1007, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 100000
-  ]);
+  const [list, setList] = useState ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]);
 
   return(
   <SafeAreaView>
@@ -27,10 +25,46 @@ export default function App() {
       renderItem={({item, index}) => ( 
       <Text> valor: {item} posição: {index}</Text> )}
     />
-    
+
+  <View style={styles.container}>
+    <Pressable style={({pressed}) => pressed ? styles.buttonPressed : styles.button}>
+      <Text style={styles.buttonText}>Press me</Text>
+
+    </Pressable>
+  </View> 
   
   </SafeAreaView>
 
   
   );
 }
+
+const styles =  StyleSheet.create({
+  button:{ 
+    backgroundColor: 'red',
+    width: 100,
+    padding: 8,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10
+  },
+  buttonPressed: {
+    backgroundColor: 'blue',
+    width: 100,
+    padding: 8,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10
+  },
+  container: {
+    margin: 16,
+  },
+  buttonText:{
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '600'
+  }
+
+});
